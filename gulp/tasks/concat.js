1cc53b8d14253e969b2fe8dcfaae5eefeb0c2e36
zygimantas <zygimantasdev@gmail.com>
2015-01-13 14:53:59 +0200

    refactor for better name

--- gulp/tasks/concat.js
+++ gulp/tasks/concat.js
@@ -3,19 +3,19 @@ var buildDestination = './build';
 var reload = require('browser-sync').reload;
 var lazypipe = require('lazypipe');
 
-var concatOutputAndReload = lazypipe()
+var outputDataAndReload = lazypipe()
     .pipe(gulp.dest, buildDestination)
     .pipe(reload, {stream:true});
 
 gulp.task('concat', function() {
     return gulp.src(scriptSource)
         .pipe($.concat('app.js'))
-        .pipe(concatOutputAndReload())
+        .pipe(outputDataAndReload())
 });
 
 gulp.task('concat.prod', function() {
     return gulp.src(scriptSource)
         .pipe($.concat('app.js'))
         .pipe($.uglify())
-        .pipe(concatOutputAndReload())
+        .pipe(outputDataAndReload())
 });
