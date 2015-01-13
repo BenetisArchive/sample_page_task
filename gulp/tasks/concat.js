aaae37501febb6a276449fbb959067ff5e79ec61
zygimantas <zygimantasdev@gmail.com>
2015-01-13 12:58:09 +0200

    javascript auto reload with browser sync

--- gulp/tasks/concat.js
+++ gulp/tasks/concat.js
@@ -1,7 +1,9 @@
 var scriptSource = './app/scripts/**/*.js';
 var buildDestination = './build';
+var reload = require('browser-sync').reload;
 gulp.task('concat', function() {
     return gulp.src(scriptSource)
         .pipe($.concat('app.js'))
-        .pipe(gulp.dest(buildDestination));
+        .pipe(gulp.dest(buildDestination))
+        .pipe(reload({stream:true}));
 });
