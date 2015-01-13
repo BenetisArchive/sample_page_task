4bd2b3458e60c43c39873a26170a5f6dc3084278
zygimantas <zygimantasdev@gmail.com>
2015-01-13 12:58:31 +0200

    error prevention for sass

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,7 +1,8 @@
 var htmlSource = './app/*.html';
 var htmlDestination = './build/';
-
+var reload = require('browser-sync').reload;
 gulp.task('copy.html', function() {
    return gulp.src(htmlSource)
-       .pipe(gulp.dest(htmlDestination));
+       .pipe(gulp.dest(htmlDestination))
+       .pipe(reload({stream:true}));
 });
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,8 +1,19 @@
 var buildDestination = './build';
 var reload = require('browser-sync').reload;
+var notify = require("gulp-notify");
 gulp.task('sass', function () {
     gulp.src('./app/styles/main.scss')
         .pipe($.sass())
+        .on('error', handleErrors)
         .pipe(gulp.dest(buildDestination))
         .pipe(reload({stream:true}));
 });
+
+function handleErrors(errorType) {
+    var args = Array.prototype.slice.call(arguments);
+    notify.onError({
+        title: "SASS compiler error",
+        message: "<%= error.message %>"
+    }).apply(this, args);
+    this.emit('end');
+}
