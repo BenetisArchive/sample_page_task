04c8a8da49154e8f004f68316778ce5c8bb84fac
zygimantas <zygimantasdev@gmail.com>
2015-01-20 13:11:07 +0200

    hack to see if page being loaded is responsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,19 @@
 (function($) {
+
+    //hack to see if page is loaded as responsive
+    var isResponsive = $("#screen").css("font-weight") == "bold";
+
+    $.fn.dropdown = function() {
+        var menu = $(this);
+        var menuLinks = menu.children("li");
+
+        menuLinks.on("click", function() {
+            console.log(this);
+        })
+    };
+
+
+    $('.menu').dropdown();
+
     $('.menu').dropit({ action: 'mouseenter' });
 })(jQuery);
